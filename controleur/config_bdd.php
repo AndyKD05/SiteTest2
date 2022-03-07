8be3a9da28e9328dd96250885a21a03938759c9b
AndyKD05 <39829615+AndyKD05@users.noreply.github.com>
2022-03-07 22:06:03 +0100

    comit bdd hebergeur

--- controleur/config_bdd.php
+++ controleur/config_bdd.php
@@ -1,7 +1,13 @@
 <?php
+/*
 	$serveur ="localhost:3307";
 	$bdd = "neigesoleil";
 	$user="root";
 	$mdp="";
+*/
+	$serveur ="db5006845924.hosting-data.io";
+	$bdd = "dbs5651848";
+	$user="dbu2909969";
+	$mdp="Wanuke70";
 
 ?>
