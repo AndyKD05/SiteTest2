1c8cb01ca1a574d09c1765068630521bfff1bf11
AndyKD05 <39829615+AndyKD05@users.noreply.github.com>
2022-03-07 22:50:27 +0100

    comit bdd selon env.

--- controleur/config_bdd.php
+++ controleur/config_bdd.php
@@ -1,13 +1,20 @@
-<?php
-/*
-	$serveur ="localhost:3307";
-	$bdd = "neigesoleil";
-	$user="root";
-	$mdp="";
-*/
-	$serveur ="db5006845924.hosting-data.io";
-	$bdd = "dbs5651848";
-	$user="dbu2909969";
-	$mdp="Wanuke70";
+<?php   
+
+	$serveur = $_SERVER['SERVER_NAME']; 
+	if ($_SERVER['SERVER_NAME']=="127.0.0.1" || $_SERVER['SERVER_NAME']=="1localhost")
+	   { 
+		$serveur ="localhost:3307";
+		$bdd = "neigesoleil";
+		$user="root";
+		$mdp="";
+	   } 
+	  else
+	   {
+	        
+		$serveur ="db5006845924.hosting-data.io";
+		$bdd = "dbs5651848";
+		$user="dbu2909969";
+		$mdp="Wanuke70";	
+	  }
 
 ?>
